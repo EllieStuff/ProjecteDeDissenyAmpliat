dfa4b070dcabdc04e6ccf14acb4c24647f50a3f5
Pau <pau.masip@enti.cat>
2022-01-13 20:27:32 +0100

    Arrows Added to Formula

--- ProjecteAmpliacioDeDisseny/Assets/MRUATextScript.cs
+++ ProjecteAmpliacioDeDisseny/Assets/MRUATextScript.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class MRUATextScript : MonoBehaviour
 {
-    private const string MRUA_DEFAULT_TEXT = "D.x = Do.x + Vo.x * t <br>" +
-                                             "D.y = Do.y + Vo.y * t + 1/2 * g * t^2";
+    private const string MRUA_DEFAULT_TEXT = "D.x = Do.x + Vo.x * T <br>" +
+                                             "D.y = Do.y + Vo.y * T + 1/2 * G * T^2";
     
     private PlayerManagerScript playerManager;
     private ValuesRecorder recorder;
     private TextMeshProUGUI text;
+
+    private Image hForce;
+    private Image vForce;
 
     Vector2 initVel;
     float timePassed = 0.0f;
@@ -22,6 +26,9 @@ public class MRUATextScript : MonoBehaviour
         recorder = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<ValuesRecorder>();
 
         text = GetComponent<TextMeshProUGUI>();
+
+        hForce = GameObject.Find("Horizontal Force").transform.GetChild(1).GetComponent<Image>();
+        vForce = GameObject.Find("Vertical Force").transform.GetChild(1).GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -35,7 +42,7 @@ public class MRUATextScript : MonoBehaviour
         }
         else
         {
-
+            updateGUIFormula();
         }
 
     }
@@ -51,15 +58,15 @@ public class MRUATextScript : MonoBehaviour
                 break;
 
             case PlayerManagerScript.State.EDITING_POS:
-                text.text = "D.x = " + playerManager.InitPos.x.ToString("F2") + " + Vo.x * t <br>" +
-                            "D.y = " + playerManager.InitPos.y.ToString("F2") + " + Vo.y * t + 1/2 * g * t^2";
+                text.text = "D.x = " + playerManager.InitPos.x.ToString("F2") + " + Vo.x * T <br>" + System.Environment.NewLine + System.Environment.NewLine +
+                            "D.y = " + playerManager.InitPos.y.ToString("F2") + " + Vo.y * T + 1/2 * G * T^2";
 
                 break;
 
             case PlayerManagerScript.State.EDITING_FORCE:
                 Vector2 initVelProjected = playerManager.InitForce / playerManager.CurrItemMass;
-                text.text = "D.x = " + playerManager.InitPos.x.ToString("F2") + " + " + initVelProjected.x.ToString("F2") + " * t <br>" +
-                            "D.y = " + playerManager.InitPos.y.ToString("F2") + " + " + initVelProjected.y.ToString("F2") + " * t + 1/2 * g * t^2";
+                text.text = "D.x = " + playerManager.InitPos.x.ToString("F2") + " + " + initVelProjected.x.ToString("F2") + " * T <br>" + System.Environment.NewLine + System.Environment.NewLine +
+                            "D.y = " + playerManager.InitPos.y.ToString("F2") + " + " + initVelProjected.y.ToString("F2") + " * T + 1/2 * G * T^2";
 
                 break;
 
@@ -76,15 +83,16 @@ public class MRUATextScript : MonoBehaviour
                 break;
 
             case PlayerManagerScript.State.WAITING_FOR_THROW:
-                text.text = playerManager.CurrItemPos.x.ToString("F2") + " = " + playerManager.InitPos.x.ToString("F2") + " + " + initVel.x.ToString("F2") + " * " + timePassed.ToString("F2") + " <br>" +
-                            playerManager.CurrItemPos.y.ToString("F2") + " = " + playerManager.InitPos.y.ToString("F2") + " + " + initVel.y.ToString("F2") + " * " + timePassed.ToString("F2") + " + 1/2 * g * " + timePassed.ToString("F2") + "^2";
+                text.text = playerManager.CurrItemPos.x.ToString("F2") + " = " + playerManager.InitPos.x.ToString("F2") + " + " + initVel.x.ToString("F2") + " * " + timePassed.ToString("F2") + " <br>" + System.Environment.NewLine + System.Environment.NewLine +
+                            playerManager.CurrItemPos.y.ToString("F2") + " = " + playerManager.InitPos.y.ToString("F2") + " + " + initVel.y.ToString("F2") + " * " + timePassed.ToString("F2") + " + 1/2 * 9.8 * " + timePassed.ToString("F2") + "^2";
 
                 timePassed += Time.deltaTime;
 
                 break;
 
             case PlayerManagerScript.State.THROW_DONE:
-
+                hForce.transform.parent.gameObject.SetActive(false);
+                vForce.transform.parent.gameObject.SetActive(false);
                 break;
 
             default:
@@ -92,6 +100,15 @@ public class MRUATextScript : MonoBehaviour
 
         }
 
+    }
+
+    public void updateGUIFormula()
+    {
+        text.text = "D.x = Do.x +        * T <br><br>" +
+                    "D.y = Do.y +        * T + 1/2 * G * T^2";
+
+        hForce.fillAmount = playerManager.forceXSlider.value;
+        vForce.fillAmount = playerManager.forceYSlider.value;
     }
 
 }
